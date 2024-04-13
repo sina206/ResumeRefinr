feea74fa94416decb092838cacc06605e2c51d09
matangolani <matgolani@gmail.com>
2024-04-13 19:27:26 +0100

    "Removed Default Home Page"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
+import Button from "@mui/material/Button";
 import "../App.css";
 
 function Home() {
@@ -9,25 +8,9 @@ function Home() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button variant="contained">Recruiter Mode</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
     </>
   );
 }
---
+++ src/pages/Recruitermode.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Recruitermode = () => {
+  return (
+    <div>Recruitermode</div>
+  )
+}
+
+export default Recruitermode
